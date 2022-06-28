5e42f564be16c73c884ae680b5744672488703da
AlxndrThOk <alex.tran24@ihsd.us>
2022-06-27 23:05:55 -0400

    Rectangle

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     {
         for (int i = 1; i <= 5; i++)
         {
-            for (int j = i; j <= 5; j++)
+            for (int j = 1; j <= 5; j++)
             {
                 System.out.print("*");
             }
