3d42200d651170e620b880bd43bb3b8d246ab077
AlxndrThOk <alex.tran24@ihsd.us>
2022-06-27 23:04:43 -0400

    Ascending Triangle

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args)
     {
-        for (int i = 0; i * 2 <= 10; i++)
+        for (int i = 1; i <= 5; i++)
         {
-            System.out.println(10 - i * 2);
+            for (int j = 6 - i; j <= 5; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
